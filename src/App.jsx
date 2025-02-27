78baddb9cd4017c0ef6e5e951fc58ee38229a0a8
daaron678 <dylan.cc.aaron@gmail.com>
2025-02-27 12:09:20 -0500

    Added button styling in index.html, added about.jsx file for implementation of Button component in React, imported about.jsx to Apps.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Map from './components/Map';
 import Panel from './components/Panel';
+// import Button_abt from './components/aboutButton';
 
 const App = () => {
   return (
@@ -11,6 +12,10 @@ const App = () => {
         <Panel />
         <div className="flex-grow">
           <Map />
+          {/* look at style choices in TailwindCSS to define className for Button*/}
+          {/* <div className="flex flex-grow">
+            <Button_abt />
+          </div> */}
         </div>
       </div>
     </div>
