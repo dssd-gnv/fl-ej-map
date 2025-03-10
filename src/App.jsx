b15be0be5b58f8a6ef721bf27f771fd36ee90f48
aaditkamat <plunges_abiotic.0q@icloud.com>
2025-03-09 20:11:43 -0400

    Read CSV data as Map state

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import {useState} from "react";
 import Header from './components/Header';
 import Map from './components/Map';
 
-
 const App = () => {
+  const datasetToFilenameMapping = {
+    'Hazardous Waste Sites': './data/hazardous_sites.csv',
+    'Superfund Sites': './data/superfund_sites.csv',
+  }
   const [datasets, updateDatasets] = useState([]);
   return (
     <div className="h-screen flex flex-col">
       <Header datasets={datasets} updateDatasets={updateDatasets} />
-      <Map datasets={datasets} />
+      <Map datasetFilenames={datasets.map((dataset) => datasetToFilenameMapping[dataset])} />
     </div>
   );
 };
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,36 @@
+import * as d3 from "d3";
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
+import {useEffect, useState} from "react";
 
-const Map = ({ datasets }) => {
+const Map = ({ datasetFilenames }) => {
+  const [siteLocations, setSiteLocations] = useState([]);
+  useEffect(() => {
+    datasetFilenames?.forEach(async (datasetFilename) => {
+      const data = await d3.csv(datasetFilename);
+      if (datasetFilename.search(/hazardous_sites/g)) {
+        setSiteLocations((prevState) => {
+          const newData = data.map((site) => {
+            return {
+              'coordinates': [parseFloat(site.X_Coordinate), parseFloat(site.Y_Coordinate)],
+              'type': 'Hazardous Waste Site'
+            }
+          });
+          return [...prevState, ...newData];
+        });
+      } else {
+        setSiteLocations((prevState) => {
+          const newData = data.map((site) => {
+            return {
+              'coordinates':[parseFloat(site.X_Coordinate), parseFloat(site.Y_Coordinate)],
+              'type': 'Superfund Site'
+            }
+          });
+          return [...prevState, ...newData];
+        });
+      }
+    });
+  }, [datasetFilenames]);
   return (
     <MapContainer className="h-full z-0" center={[28.54, -81.65]} zoom={7} style={{ height: "100%", width: "100%" }}>
       <TileLayer
