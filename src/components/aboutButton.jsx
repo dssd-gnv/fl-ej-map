90cbb80da2d7e4157114fb0bf345d30e365c22fc
daaron678 <dylan.cc.aaron@gmail.com>
2025-03-25 13:23:49 -0400

    button functionality

--- src/components/aboutButton.jsx
+++ src/components/aboutButton.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 
-
-
-
+export default function About() {
+  return (
+    <>
+      <Button href="https://gainesville.dssdglobal.org/projects/ej-dashboard" variant = "outline-primary" value= "About"/>
+    </>
+  );
+}
 
 
 
