caa47c5dd6b7337dfc5e34f0e32c070fd2d22b6a
Aadit Kamat <plunges_abiotic.0q@icloud.com>
2025-04-02 01:29:57 -0400

    Add enhanced UI for sidebar with different types of user actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import {useState} from "react";
-import Header from './components/Header';
 import Map from './components/Map';
+import Panel from './components/Panel';
 
 const App = () => {
-  const datasetToFilenameMapping = {
-    'Hazardous Waste Sites': './data/hazardous_sites_small.csv',
-    'Superfund Sites': './data/superfund_sites.csv',
-  }
-  const [datasets, updateDatasets] = useState([]);
+  const [data, setData] = useState([]);
   return (
-    <div className="h-screen flex flex-col">
-      <Header datasets={datasets} updateDatasets={updateDatasets} />
-      <Map datasetFilenames={datasets.map((dataset) => datasetToFilenameMapping[dataset])} />
+    <div className="h-screen flex flex-col flex-grow">
+        <div className="flex flex-grow">
+            <Panel setData={setData}/>
+            <div className="flex-grow">
+                <Map data={data} />
+            </div>
+        </div>
     </div>
   );
 };
